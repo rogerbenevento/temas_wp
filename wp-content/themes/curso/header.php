f1d3b92bf90197c2f1885323c2ad1a4cac7871a1
Rogerio Benevento Jr <rogerbenevento@gmail.com>
2018-04-15 11:50:58 -0300

    Criada estrutura do tema e separado em arquivos de header e footer

---
+++ wp-content/themes/curso/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Curso Wordpress</title>
+</head>
+<body <?php body_class() ?>>
+    
+    <!-- Header -->
+    <header>
+        <div class="barra-topo">
+            <div class="redes-sociais"></div>
+            <div class="pesquisa"></div>
+        </div>
+
+        <div class="area-menu">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+    </header>
+    <!-- End Header -->
--- wp-content/themes/curso/index.php
+++ wp-content/themes/curso/index.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Curso Wordpress</title>
-</head>
-<body>
-    <p></p>
-</body>
-</html>
+<?php get_header() ?>
+
+    <!-- Content -->
+    <div class="conteudo">
+        <main>
+            <section class="slide"></section>
+
+            <section class="servicos"></section>
+
+            <section class="meio">
+                <aside class="barra-lateral"></aside>
+                <div class="noticias"></div>
+            </section>
+
+            <section class="mapa"></section>
+        </main>
+    </div>
+    <!-- End Content -->
+
+<?php get_footer() ?>
